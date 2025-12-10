f5f21dab5ff6a735114f80896e19fd859539ce17
muhamadzain-Ai <miancr856@gmail.com>
2025-12-10 20:28:53 +0500

    conditions.py

--- conditions.py
+++ conditions.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 marks=int(input("Enter a marks:"))
 print("Student marks is",marks)
 if(marks>=90):
@@ -9,15 +8,3 @@ elif(marks<80):
     print("Grade is C")
 else :
     print("Grade is F")
-=======
-marks=int(input("Enter a marks:"))
-print("Student marks is",marks)
-if(marks>=90):
-    print("Grade is A")
-elif(marks<90):
-    print("Grade is B")
-elif(marks<80):
-    print("Grade is C")
-else :
-    print("Grade is F")
->>>>>>> 9b53b3a4b81b1a4b81ed958c978e3b98ca2492d0
